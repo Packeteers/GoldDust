a71135b804a7833b9ab1a2bae285cc7b60117112
John Marion <john@lmsn.net>
2016-10-03 17:26:45 -0700

    Add global configuration file creation

--- golddust/__init__.py
+++ golddust/__init__.py
@@ -20,6 +20,7 @@ Minecraft Forge and texture packs).
 """
 
 
+import json
 import os
 import platform
 
@@ -44,15 +45,6 @@ def default_home_dir():
     return os.path.abspath(os.path.expanduser(path))
 
 
-def write_default_config(path):
-    """Write out a default GoldDust configuration.
-    """
-    config = open(path, "w+")
-    # TODO Actually serialize a configuration
-    config.write("{}")
-    config.close()
-
-
 def install_home_dir(path):
     """Install the GoldDust home directory.
 
@@ -75,7 +67,81 @@ def install_home_dir(path):
     os.mkdir(gdhome, mode=0o755)
     os.mkdir(os.path.join(gdhome, "pkgcache"))
     os.mkdir(os.path.join(gdhome, "instances"))
-    write_default_config(os.path.join(gdhome, "config.json"))
+    gdust = GoldDust(gdhome)
+    gdust.save_global_config()
+
+
+class GlobalConfig:
+    """GoldDust global configuration.
+
+    This configuration holds information about repositories and their
+    mirrors.
+    """
+    def __init__(self):
+        self.repositories = []
+
+    def get_repository(self, repo):
+        """Get the repository dict from a repository name.
+
+        Takes:
+            repo (str): The repository name to get.
+
+        Raises:
+            KeyError if the repository doesn't exist in the configuration.
+
+        Returns the dict for the requested repository."""
+        target = None
+        for repo_dict in self.repositories:
+            if repo_dict["name"] == repo:
+                target = repo_dict
+
+        if not target:
+            raise KeyError("Repository doesn't exist in configuration.")
+
+        return target
+
+    def add_repository(self, name, mirrors):
+        """Add a repository to the global configuration.
+
+        Takes:
+            name (str): The name for this repository. Traditionally
+                        alphanumeric-only in lowercase.
+            mirrors (list of str): HTTP(S) mirrors of this repository.
+
+        Raises:
+            ValueError: The repository already exists in the configuration.
+        """
+        for repo in self.repositories:
+            if repo["name"] == name:
+                raise ValueError("Repository already exists in configuration.")
+
+        self.repositories.append({"name": name, "mirrors": mirrors})
+
+    def remove_repository(self, name):
+        """Remove a repository from the GoldDust configuration.
+
+        Takes:
+            name (str): The name of the repository to remove.
+
+        Raises:
+            KeyError: The repository does not exist in the configuration.
+        """
+        self.repositories.remove(self.get_repository(name))
+
+    def add_mirror(self, repo, mirror):
+        """Add a mirror to the configuration for a repository.
+
+        Takes:
+            repo (str): The repo name to add the mirror to.
+            mirror (str): The HTTP(S) URI of the mirror.
+
+        Raises:
+            KeyError: The repository doesn't exist in the configuration.
+        """
+        target = self.get_repository(repo)["mirrors"]
+        if mirror in target:
+            raise KeyError("Mirror already exists for repository.")
+        target.append(mirror)
 
 
 class Package:
@@ -93,3 +159,23 @@ class Package:
     def sig_file(self):
         """The detached signature file name for this package."""
         return "{}.sig".format(self.tarball)
+
+
+class GoldDust:
+    """An instance of the GoldDust package manager."""
+    def __init__(self, root):
+        self.config = GlobalConfig()
+        self.root = root
+
+        if os.path.isfile(os.path.join(self.root, "config.json")):
+            self.load_global_config()
+
+    def save_global_config(self):
+        """Save the global configuration."""
+        config = open(os.path.join(self.root, "config.json"), "w+")
+        json.dump(self.config.__dict__, config, sort_keys=True, indent=4)
+        config.close()
+
+    def load_global_config(self):
+        """Load the global configuration from a file."""
+        pass
